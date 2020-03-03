23daadb1e5f70685d9e6a1b4f81e252ec0dd684a
Constance Mahony <constance.mahony.16@ucl.ac.uk>
2020-03-03 11:18:53 +0000

    fixed some hod parameters to SDSS fiducial values - not enough info in LF to constrain all of them

--- luminosity_function_fiducial_values/plot_emcee_chains.py
+++ luminosity_function_fiducial_values/plot_emcee_chains.py
@@ -1,21 +1,16 @@
 import numpy as np
 from chainconsumer import ChainConsumer
 
-#mean = [0.0, 4.0]
-data_file = '/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/luminosity_function_fiducial_values/output/LF.txt'
+data_file = '/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/luminosity_function_fiducial_values/output/LF_some_values_fixed.txt'
 data = np.loadtxt(data_file)
 
 samples = data[:,:-1]
 
-with open(data_file) as f:
-    first_line = f.readline()
-
-print(len(first_line[1:].split('\t')))
-
-parameter_list = [value for index, value in enumerate(first_line[1:].split('\t')[:-1])]
-print(parameter_list)
+#data2 = np.loadtxt('/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/luminosity_function_fiducial_values/output/LF.txt')
+#samples2 = data2[:,:-1]
 
 c = ChainConsumer()
-c.add_chain(samples, parameters= ["lgm1", "lgl0", "g1", "g2", "scatter", "alfas", "b0", "b1", "b2"])
+c.add_chain(samples, parameters= ["lgm1", "lgl0", "scatter", "b0"]) #["lgm1", "lgl0", "g1", "g2", "scatter", "alfas", "b0", "b1", "b2"])
+#c.add_chain(samples2, parameters= ["lgm1", "lgl0", "g1", "g2", "scatter", "alfas", "b0", "b1", "b2"])
 c.configure(kde=[True]) #, shade_alpha=0.1, flip=False)
-c.plotter.plot(filename="LF_emcee.png", figsize="column")
+c.plotter.plot(filename="LF_emcee_some_values_fixed.png", figsize="column")
