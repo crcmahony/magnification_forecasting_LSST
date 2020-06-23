5e5ffd0b244084a50700609d10aa927a5340e5f7
Constance Mahony <constance.mahony.16@ucl.ac.uk>
2020-06-23 17:16:37 +0100

    luminosity function fiducial values

---
+++ luminosity_function_fiducial_values/exec_LF_job.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#PBS -q mediumc7
+#PBS -N LF_n
+#PBS -l nodes=1:ppn=1:typeb
+#PBS -l mem=20gb
+
+source /unix/atlas4/akorn/LSST/cosmosis/cosmosis/setup-my-cosmosis
+
+cosmosis /unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/luminosity_function_fiducial_values/LF.ini
--- luminosity_function_fiducial_values/plot_emcee_chains.py
+++ luminosity_function_fiducial_values/plot_emcee_chains.py
@@ -1,5 +1,6 @@
 import numpy as np
 from chainconsumer import ChainConsumer
+import matplotlib.pyplot as plt
 
 data_file = '/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/luminosity_function_fiducial_values/output/LF_some_values_fixed.txt'
 data = np.loadtxt(data_file)
@@ -10,7 +11,15 @@ samples = data[:,:-1]
 #samples2 = data2[:,:-1]
 
 c = ChainConsumer()
-c.add_chain(samples, parameters= ["lgm1", "lgl0", "scatter", "b0"]) #["lgm1", "lgl0", "g1", "g2", "scatter", "alfas", "b0", "b1", "b2"])
+c.add_chain(samples, parameters= ["lgm1", "lgl0", "scatter", "b0"], walkers=40) #["lgm1", "lgl0", "g1", "g2", "scatter", "alfas", "b0", "b1", "b2"])
 #c.add_chain(samples2, parameters= ["lgm1", "lgl0", "g1", "g2", "scatter", "alfas", "b0", "b1", "b2"])
 c.configure(kde=[True]) #, shade_alpha=0.1, flip=False)
-c.plotter.plot(filename="LF_emcee_some_values_fixed.png", figsize="column")
+
+
+#gelman_rubin_converged = c.diagnostic.gelman_rubin()
+geweke_converged = c.diagnostic.geweke()
+print(geweke_converged)
+
+fig = c.plotter.plot(filename="LF_emcee_some_values_fixed_with_kde.pdf", figsize="column")
+
+#fig.patch.set_facecolor('white')
