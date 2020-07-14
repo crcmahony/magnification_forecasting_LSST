126b158df3557f01baa15f34a9f73330bc6de26a
Constance Mahony <constance.mahony.16@ucl.ac.uk>
2020-07-14 18:34:44 +0100

    added shear step sizes

--- clustering/fisher_step_sizes/paper_plot_fisher_grid_many.py
+++ clustering/fisher_step_sizes/paper_plot_fisher_grid_many.py
@@ -22,10 +22,10 @@ def calculate_fisher_std_dev(fisher_dir, optimum_step_size_fisher_filename):
         std_dev = (fisher**(-1.))**0.5
         return std_dev
 
-fisher_dir = '/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/clustering/fisher_step_sizes/fisher_jobs_clustering_mag/'
-grid_dir= '/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/clustering/fisher_step_sizes/grid_jobs_clustering_mag/'
-step_size_choice = 'clustering_mag'
-plot_type = 'overlay' #'percentage_diff'
+fisher_dir = '/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/clustering/fisher_step_sizes/fisher_jobs_shear/'
+grid_dir= '/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/clustering/fisher_step_sizes/grid_jobs_shear/'
+step_size_choice = 'shear'
+plot_type = 'percentage_diff' #'overlay'
 
 grid_files = [i for i in listdir(grid_dir) if i.endswith('grid_out.txt')]
 params = [i.replace('_grid_out.txt', '') for i in grid_files]
@@ -171,6 +171,50 @@ if step_size_choice == 'clustering_mag_gold':
         fish_files['magnification_bias--alpha_m'] = 'magnification_bias--alpha_m_fisher_out0005.txt'
         fish_files['magnification_bias--beta_m'] = 'magnification_bias--beta_m_fisher_out0005.txt'
 
+if step_size_choice == 'shear':
+        fish_files['cosmological_parameters--omega_m'] = 'cosmological_parameters--omega_m_fisher_out0005.txt'
+        fish_files['cosmological_parameters--h0'] = 'cosmological_parameters--h0_fisher_out0022.txt'
+        fish_files['cosmological_parameters--omega_b'] = 'cosmological_parameters--omega_b_fisher_out0022.txt'
+        fish_files['cosmological_parameters--n_s'] = 'cosmological_parameters--n_s_fisher_out0022.txt'
+        fish_files['cosmological_parameters--e9A_s'] = 'cosmological_parameters--e9A_s_fisher_out0022.txt'
+        fish_files['cosmological_parameters--w'] = 'cosmological_parameters--w_fisher_out0022.txt'
+        fish_files['cosmological_parameters--wa'] = 'cosmological_parameters--wa_fisher_out0215.txt'
+
+        fish_files['hod_parameters--lgM1'] = 'hod_parameters--lgM1_fisher_out0005.txt'
+        fish_files['hod_parameters--lgl0'] = 'hod_parameters--lgl0_fisher_out0005.txt'
+        fish_files['hod_parameters--g1'] = 'hod_parameters--g1_fisher_out0005.txt'
+        fish_files['hod_parameters--g2'] = 'hod_parameters--g2_fisher_out0005.txt'
+        fish_files['hod_parameters--scatter'] = 'hod_parameters--scatter_fisher_out0005.txt'
+        fish_files['hod_parameters--alfa_s'] = 'hod_parameters--alfa_s_fisher_out0005.txt'
+        fish_files['hod_parameters--b0'] = 'hod_parameters--b0_fisher_out0005.txt'
+        fish_files['hod_parameters--b1'] = 'hod_parameters--b1_fisher_out0005.txt'
+        fish_files['hod_parameters--b2'] = 'hod_parameters--b2_fisher_out0005.txt'
+
+        fish_files['nz_e_sample_errors--bias_1'] = 'nz_e_sample_errors--bias_1_fisher_out0005.txt'
+        fish_files['nz_e_sample_errors--bias_2'] = 'nz_e_sample_errors--bias_2_fisher_out0005.txt'
+        fish_files['nz_e_sample_errors--bias_3'] = 'nz_e_sample_errors--bias_3_fisher_out0005.txt'
+        fish_files['nz_e_sample_errors--bias_4'] = 'nz_e_sample_errors--bias_4_fisher_out0005.txt'
+        fish_files['nz_e_sample_errors--bias_5'] = 'nz_e_sample_errors--bias_5_fisher_out0005.txt'
+        fish_files['nz_e_sample_errors--bias_6'] = 'nz_e_sample_errors--bias_6_fisher_out0005.txt'
+        fish_files['nz_e_sample_errors--bias_7'] = 'nz_e_sample_errors--bias_7_fisher_out0005.txt'
+        fish_files['nz_e_sample_errors--bias_8'] = 'nz_e_sample_errors--bias_8_fisher_out0005.txt'
+        fish_files['nz_e_sample_errors--bias_9'] = 'nz_e_sample_errors--bias_9_fisher_out0005.txt'
+        fish_files['nz_e_sample_errors--bias_10'] = 'nz_e_sample_errors--bias_10_fisher_out0005.txt'
+
+        fish_files['ia_parameters--gamma_2h'] = 'ia_parameters--gamma_2h_fisher_out0005.txt'
+
+        fish_files['shear_calibration_parameters--m1'] = 'shear_calibration_parameters--m1_fisher_out0005.txt'
+        fish_files['shear_calibration_parameters--m2'] = 'shear_calibration_parameters--m2_fisher_out0005.txt'
+        fish_files['shear_calibration_parameters--m3'] = 'shear_calibration_parameters--m3_fisher_out0005.txt'
+        fish_files['shear_calibration_parameters--m4'] = 'shear_calibration_parameters--m4_fisher_out0005.txt'
+        fish_files['shear_calibration_parameters--m5'] = 'shear_calibration_parameters--m5_fisher_out0005.txt'
+        fish_files['shear_calibration_parameters--m6'] = 'shear_calibration_parameters--m6_fisher_out0005.txt'
+        fish_files['shear_calibration_parameters--m7'] = 'shear_calibration_parameters--m7_fisher_out0005.txt'
+        fish_files['shear_calibration_parameters--m8'] = 'shear_calibration_parameters--m8_fisher_out0005.txt'
+        fish_files['shear_calibration_parameters--m9'] = 'shear_calibration_parameters--m9_fisher_out0005.txt'
+        fish_files['shear_calibration_parameters--m10'] = 'shear_calibration_parameters--m10_fisher_out0005.txt'
+
+
 if step_size_choice == 'joint_gold':
         fish_files['cosmological_parameters--omega_m'] = 'cosmological_parameters--omega_m_fisher_out0005.txt'
         fish_files['cosmological_parameters--h0'] = 'cosmological_parameters--h0_fisher_out0010.txt'
@@ -304,7 +348,7 @@ f.set_size_inches(18, 10)
 plt.tight_layout()
 #plt.subplots_adjust(wspace=0)
 plt.legend()
-plt.savefig('/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/clustering/fisher_step_sizes/plots/fisher_grid_matched_clustering_mag.png')
+plt.savefig('/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/clustering/fisher_step_sizes/plots/fisher_grid_matched_shear_diff.png')
 plt.show()
 
 
