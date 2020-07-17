51811166226851773a02d98b2485794f899426e6
Constance Mahony <constance.mahony.16@ucl.ac.uk>
2020-07-17 13:12:33 +0100

    added script for including priors in shear and clustering

--- priors/add_priors.py
+++ priors/add_priors.py
@@ -11,11 +11,22 @@ def generate_clustering_prior_matrix(sample):
 
         return prior_matrix
 
+def generate_shear_clustering_gold_prior_matrix():
+        prior_matrix = np.zeros((39,39))
+        
+        for i in np.arange(16, 26, 1):
+                        prior_matrix[i,i] = 1/0.001**2.0
 
-F_without_priors = np.loadtxt('/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/clustering_mag/output/fisher_clustering_mag3.txt')
-F_type = 'clustering'
-sample = 'faint'
-filename = '/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/priors/Fisher_matrices_with_priors/fisher_clustering_mag_priors3.txt'
+        for i in np.arange(27, 37, 1):
+                        prior_matrix[i,i] = 1/0.003**2.0
+
+        return prior_matrix
+
+
+F_without_priors = np.loadtxt('/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/shear_clustering/fisher_shear_plus_clustering/shear_clustering_mag_gold.txt')
+F_type = 'shear_clustering_gold'
+sample = 'faint' #required for clustering
+filename = '/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/priors/shear_clustering_fisher_matrices_with_priors/fisher_shear_clustering_mag_gold_priors.txt'
 
 #F_test = np.loadtxt('/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/clustering/output/fisher_clustering_nz_priors.txt')
 
@@ -24,6 +35,11 @@ if F_type == 'clustering':
         F_with_priors = F_without_priors + prior_matrix
 
         #print(np.array_equal(F_with_priors, F_test))
+        np.savetxt(filename, F_with_priors)
+
+if F_type == 'shear_clustering_gold':
+        prior_matrix = generate_shear_clustering_gold_prior_matrix()
+        F_with_priors = F_without_priors + prior_matrix
         np.savetxt(filename, F_with_priors)
 
 
