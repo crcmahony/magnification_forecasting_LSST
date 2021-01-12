7e31b8d98a8fc5e3867798f801879ca21df0f5ce
Constance Mahony <constance.mahony.16@ucl.ac.uk>
2021-01-12 10:41:09 +0000

    remove LF fiducial vals

--- luminosity_function_fiducial_values/exec_LF_e_sample_job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#PBS -q mediumc7
-#PBS -N LF_e
-#PBS -l nodes=1:ppn=1:typeb
-#PBS -l mem=20gb
-
-source /unix/atlas4/akorn/LSST/cosmosis/cosmosis/setup-my-cosmosis
-
-cosmosis /unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/luminosity_function_fiducial_values/LF_e_sample.ini
