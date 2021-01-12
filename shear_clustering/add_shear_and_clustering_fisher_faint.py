6cf1774fe2c4c883e78d9305633f7b1990045c57
Constance Mahony <constance.mahony.16@ucl.ac.uk>
2021-01-12 11:02:47 +0000

    tidy shear clustering

--- shear_clustering/add_shear_and_clustering_fisher_faint.py
+++ shear_clustering/add_shear_and_clustering_fisher_faint.py
@@ -6,29 +6,29 @@ import numpy as np
 shear = np.loadtxt('/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/shear/output/fisher_shear.txt')
 shear_zeros = np.zeros((49,49))
 
-shear_zeros[:37, :37] = shear
+shear_zeros[:28, :28] = shear
 
 np.savetxt('shear_zeros_faint.txt', shear_zeros)
 
 
 
-clustering = np.loadtxt('/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/clustering_mag/output/fisher_clustering_mag3.txt')
+clustering = np.loadtxt('/unix/atlas4/akorn/LSST/cosmosis/cosmosis/modules/euclid_ias/demos/thesis_results/clustering_mag/output/fisher_clustering_mag.txt')
 print(clustering.shape)
 
 clustering_zeros_col = np.zeros((28, 21))
 print(clustering_zeros_col.shape)
-clustering_columns_added = np.hstack((clustering[:,:16], clustering_zeros_col, clustering[:, 16:]))
+clustering_columns_added = np.hstack((clustering[:,:7], clustering_zeros_col, clustering[:, 7:]))
 print(clustering_columns_added.shape)
 
 clustering_zeros_row = np.zeros((21,49))
 print(clustering_zeros_row.shape)
-clustering_rows_added = np.vstack((clustering_columns_added[:16,:], clustering_zeros_row, clustering_columns_added[16:,:]))
+clustering_rows_added = np.vstack((clustering_columns_added[:7,:], clustering_zeros_row, clustering_columns_added[7:,:]))
 print(clustering_rows_added.shape)
 
 np.savetxt('clustering_zeros_faint.txt', clustering_rows_added)
 
 shear_clustering_fisher = shear_zeros + clustering_rows_added
 
-np.savetxt('fisher_shear_plus_clustering/shear_clustering_mag.txt', shear_clustering_fisher)
+np.savetxt('rerun_cacciato/shear_clustering_mag.txt', shear_clustering_fisher)
 
 
